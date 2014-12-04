c2a1f2b282e6fdbee9d3da54024cfc746be3d9e4
Brian Ball <ideabrian@gmail.com>
2014-12-04 00:18:13 -0800

    LetterCountI working

---
+++ letter_count.js
@@ -0,0 +1,43 @@
+function processWord(word) {
+    word = word.toLowerCase();
+    var a = word.split('');
+    var repeatCount = 0;
+    for (var i in a) {
+        var letter = a[i];
+        var re = new RegExp('[' + letter + ']', 'g');
+        var count = word.match(re).length;
+        if (count > repeatCount) {
+            repeatCount = count;
+        }
+    }
+    return repeatCount;
+}
+
+function LetterCountI(str) {
+    var words = str.split(" ");
+    var accounting = [];
+    var winner = '';
+    
+    for (var i in words) {
+        var repeats = processWord(words[i]);
+        accounting.push([words[i], repeats]);
+    } // for
+    // process accounting
+    console.log(accounting);
+    var mostRepeats = 0;
+    for (var j = 0; j < accounting.length; j++) {
+        
+        console.log('accounting[j][1]: ' + accounting[j]);
+        if (accounting[j][1] > mostRepeats) {
+            mostRepeats = accounting[j][1];
+             winner = accounting[j][0];
+        }
+  } // for 
+    return winner;
+}
+
+
+var str = "Today, is the greatetst day ever!";
+
+    
+console.log(LetterCountI(str));
